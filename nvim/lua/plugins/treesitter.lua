383ea0be833612fad3ae69f552b862b30bb9c629
zavvdev <zaviryukha.igor99@gmail.com>
2024-01-15 12:54:14 +0200

    added comment plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
+
     config.setup({
       auto_install = true,
       highlight = { enable = true },

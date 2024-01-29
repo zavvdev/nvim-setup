0eb03eac6bc1c2c71fccb7d7000df651094da26d
zavvdev <zaviryukha.igor99@gmail.com>
2024-01-29 18:45:45 +0200

    added git diffview

---
+++ nvim/lua/plugins/git-diffview.lua
@@ -0,0 +1,10 @@
+return {
+  "sindrets/diffview.nvim",
+  config = function()
+    require("diffview").setup({
+      use_icons = false,
+    })
+    vim.keymap.set("n", "<leader>dv", ":DiffviewOpen<CR>", {})
+    vim.keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", {})
+  end,
+}

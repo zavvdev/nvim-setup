265f05352f73bc6be6c84e8582e5a1a8886a3d30
zavvdev <zaviryukha.igor99@gmail.com>
2024-05-25 13:51:15 +0300

    updated null-ls config

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,7 +8,11 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.joker,
         null_ls.builtins.formatting.pint,
-        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" })
+          end,
+        }),
       },
     })
     vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
